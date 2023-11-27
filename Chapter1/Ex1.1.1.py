6dca69a3213aa7bfe9df6d3b7ae7ffbf37c80918
frica12 <fricaa@naver.com>
2023-11-28 00:42:38 +0900

    .

---
+++ Chapter1/Ex1.1.1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = np.array([1, 2, 3])
+print(x.shape)
+print(x.ndim)
+
+W = np.array([[1, 2, 3], [4, 5, 6]])
+print(W.shape)
+print(W.ndim)
---
+++ Chapter1/Ex1.1.3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.array([[1,2], [3,4]])
+
+print(A * 10)
+
+A = np.array([[1,2], [3,4]])
+b = np.array([10, 20])
+print(A * b)
